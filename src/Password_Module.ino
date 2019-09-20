ce44647ccd0a45c2438369174176dfd0a203c096
Oryan Moshe <oryan@monday.com>
2019-09-20 15:47:23 +0300

    Added lcd control

--- src/Password_Module.ino
+++ src/Password_Module.ino
@@ -1,17 +1,19 @@
+#include <Wire.h>  
+#include <LiquidCrystal_I2C.h>
+
 const int BUTTONS[11] = {
-  12, // PASSWORD_BTN_1
-  11, // PASSWORD_BTN_2
-  10, // PASSWORD_BTN_3
-  9, // PASSWORD_BTN_4
-  8, // PASSWORD_BTN_5
-  7, // PASSWORD_BTN_6
-  6, // PASSWORD_BTN_7
-  5, // PASSWORD_BTN_8
-  4, // PASSWORD_BTN_9
-  3, // PASSWORD_BTN_10
-  
-  13 // SUBMIT_BTN
+  12, /* PASSWORD_BTN_01 */ 11, /* PASSWORD_BTN_02 */
+  10, /* PASSWORD_BTN_03 */ 9 , /* PASSWORD_BTN_04 */
+  8 , /* PASSWORD_BTN_05 */ 7 , /* PASSWORD_BTN_06 */
+  6 , /* PASSWORD_BTN_07 */ 5 , /* PASSWORD_BTN_08 */
+  4 , /* PASSWORD_BTN_09 */ 3 , /* PASSWORD_BTN_10 */
+                 13 /* SUBMIT_BTN */
 };
+
+const int ARRAY_LENGTH = sizeof(BUTTONS) / sizeof(BUTTONS[0]);
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+// the 8 arrays that form each segment of the custom numbers
 
 void passwordSetup() {
   for(int i = 0; i < 11; i++) {
