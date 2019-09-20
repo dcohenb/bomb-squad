845a16f1a40e84004a2202c19273a65db4ee14a5
Daniel Cohen <danielco@monday.com>
2019-09-20 10:17:07 +0300

    initial commit

---
+++ Strikes_Success.ino
@@ -0,0 +1,19 @@
+#define STRIKES_PIXELS_PIN 23
+#define STRIKES_PIXELS_NUM 3
+Adafruit_NeoPixel _strikesPixels(STRIKES_PIXELS_NUM, STRIKES_PIXELS_PIN, NEO_GRB + NEO_KHZ800);
+
+int strikes = 0;
+
+void strikeSetup() {
+  _strikesPixels.begin();
+  _strikesPixels.clear();
+  _strikesPixels.show();
+}
+
+void addStrike() {
+  strikes++;
+
+  _strikesPixels.clear();
+  _strikesPixels.fill(_strikesPixels.Color(150, 0, 0), 0, strikes);
+  _strikesPixels.show();
+}
